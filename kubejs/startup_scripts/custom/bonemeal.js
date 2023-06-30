68896091b06244ce42e401c0c89090051a881973
TheDrOfDoctoring <bennetanstee@gmail.com>
2023-06-30 04:28:17 +0400

    bonemeal fix

--- kubejs/startup_scripts/custom/bonemeal.js
+++ kubejs/startup_scripts/custom/bonemeal.js
@@ -2,11 +2,13 @@
 const SaplingBlock = Java.loadClass('net.minecraft.world.level.block.SaplingBlock')
 
 ForgeEvents.onEvent("net.minecraftforge.event.entity.player.BonemealEvent", event => {
-    if(event.getStack().id === "botania:living_root" && (/.*sapling/.test(event.getBlock().getBlock().id.toString()))) {
-        if(event.getBlock().getBlock() instanceof SaplingBlock && ! event.getLevel().isClientSide()) {
-            let block = event.getBlock().getBlock()
-            block.advanceTree(event.getLevel(), event.getPos(), event.getBlock(), event.getLevel().random)
-        }
+    if(event != null) {
+        if(event.getStack() != null && !(event.getStack().id === "minecraft:bonemeal") && event.getStack().id === "botania:living_root" && (/.*sapling/.test(event.getBlock().getBlock().id.toString()))) {
+            if(event.getBlock().getBlock() instanceof SaplingBlock &&  !event.getLevel().isClientSide()) {
+                let block = event.getBlock().getBlock()
+                block.advanceTree(event.getLevel(), event.getPos(), event.getBlock(), event.getLevel().random)
+            }
+        } 
     }
 })  
 
